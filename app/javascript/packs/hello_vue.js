5aa095bf9cbbdc7dd37387511856ffc272ea8d30
Takuya Nishio <littlecub240@gmail.com>
2023-09-24 00:24:16 +0900

    dynamic import Vue3

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,21 +1,21 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
 import { createApp } from 'vue';
-import Onboarding from '../Onboarding.vue';
 
 let observer;
 
-const loadAndMountVueApp = () => {
-  const appInstance = createApp(Onboarding);
-  appInstance.mount('#onboarding-container');
+const loadAndMountVueApp = async () => {
+  try {
+    // App.vueを動的にインポート
+    const { default: App } = await import('../Onboarding.vue');
+    
+    const appInstance = createApp(App);
+    appInstance.mount('#onboarding-container');
 
-  // Observerを解除
-  if (observer) {
-    observer.disconnect();
+    // Observerを解除
+    if (observer) {
+      observer.disconnect();
+    }
+  } catch (error) {
+    console.error("Failed to load App.vue:", error);
   }
 };
 
